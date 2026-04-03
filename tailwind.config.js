cc143feda589166d5b9883ebe10889c3b1c51d7b
lokiiiofficial19 <lokiiiofficial1337@gmail.com>
2026-04-03 13:09:57 +0530

    add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#6C63FF",
+        secondary: "#6B7280",
+      },
+    },
+  },
+  plugins: [],
+};
